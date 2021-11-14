ac86ecd17890c366110ccd494374daa722d45e5d
Ivan Martinez Laborda <ivan@Macbook-Pro-Ivan-personal.local>
2021-11-14 18:44:27 +0100

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
-/**
- * Required External Modules
- */
-
 import express, { Application } from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 import helmet from 'helmet'
 
 import router from './routes'
-
-/**
- * App Variables
- */
 
 dotenv.config()
 
@@ -22,17 +14,9 @@ if (!process.env.PORT) {
 const PORT: number = parseInt(process.env.PORT as string, 10)
 const app: Application = express()
 
-/**
- *  App Configuration
- */
-
 app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
-
-/**
- * Server Activation
- */
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}!`))
